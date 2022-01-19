08cfeb27cd840e71ae64172bee943ebfd0f06761
anurag-bhadauriya <ab02091995@gmail.com>
2022-01-20 00:42:40 +0530

    Boilerplate code updated

---
+++ CppHelloWorld.cpp
@@ -0,0 +1,12 @@
+//============================================================================
+// C++ Boilerplate Code
+//============================================================================
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	cout << "Hello World!";
+	return 0;
+}
